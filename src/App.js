001e158aab80a0db729b6cacc47adfc4a6454cae
andrey <ovcharenkoav@meta.ua>
2018-11-14 16:33:02 +0200

    post create form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
+import { PostList, PostCreate } from './posts';
 import { UserList } from './users';
 
 import jsonServerProvider from 'ra-data-json-server';
@@ -10,7 +10,7 @@ const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
-        <Resource name="posts" list={PostList} edit={EditGuesser} />
+        <Resource name="posts" list={PostList} edit={EditGuesser} create={PostCreate} />
         <Resource name="users" list={UserList} edit={EditGuesser}/>
     </Admin>
 );
--- src/posts.js
+++ src/posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField } from 'react-admin';
+import { List, Datagrid, TextField, ReferenceField, SimpleForm, ReferenceInput, SelectInput, LongTextInput, TextInput, Create } from 'react-admin';
 
 export const PostList = props => (
     <List {...props}>
@@ -12,4 +12,16 @@ export const PostList = props => (
             <TextField source="body" />
         </Datagrid>
     </List>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Create>
 );
